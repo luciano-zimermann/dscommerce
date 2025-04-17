38b3bfd50cccbcbcdbb8bf5be9c898d6270072b0
Luciano Zimermann <lucianozn16@gmail.com>
2025-04-16 21:39:07 -0300

    implementing search by name of products

--- src/main/java/com/lucianozimermann/dscommerce/repositories/ProductRepository.java
+++ src/main/java/com/lucianozimermann/dscommerce/repositories/ProductRepository.java
@@ -1,7 +1,14 @@
 package com.lucianozimermann.dscommerce.repositories;
 
 import com.lucianozimermann.dscommerce.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long>
-{}
+{
+    @Query("SELECT p FROM Product p WHERE " +
+           "UPPER(p.name) LIKE UPPER(CONCAT('%', :name, '%'))")
+    Page<Product> searchByName( String name, Pageable pageable );
+}
--- src/main/java/com/lucianozimermann/dscommerce/services/ProductService.java
+++ src/main/java/com/lucianozimermann/dscommerce/services/ProductService.java
@@ -30,9 +30,9 @@ public class ProductService
     }
 
     @Transactional( readOnly = true )
-    public Page<ProductDTO> findAll( Pageable pageable )
+    public Page<ProductDTO> findAll( String name, Pageable pageable )
     {
-        Page<Product> result = repository.findAll( pageable );
+        Page<Product> result = repository.searchByName( name, pageable );
 
         return result.map( ProductDTO::new );
     }
